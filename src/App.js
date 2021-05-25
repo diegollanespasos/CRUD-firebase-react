689522673c49d8a3af539448a9f70fb48022581a
diegollanespasos <diego.llanes@theksquaregroup.com>
2021-05-24 20:18:41 -0500

    added firebase configuration. Added add and get functionality with firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { db } from './helpers/db';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const saveUser = async () => {
+    try {
+      const newUser = {
+        name,
+        email,
+        age,
+      }
+      await db.collection('users').add(newUser);
+
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  const showUsers = async () => {
+    try {
+      const snapshot = await db.collection('users').get();
+      const docs = [];
+      snapshot.forEach(doc =>{
+        const id = doc.id;
+        const data = doc.data();
+        docs.push({id, ...data})
+      })
+      setUsers(docs);
+      console.log(users);
+    } catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      type='text'
+      onChange={(e) => setName(e.target.value) }
+      />
+      <input
+      type='email'
+      onChange={(e) => setEmail(e.target.value) }
+      />
+      <input
+      type='number'
+      onChange={(e) => setAge(e.target.value) }
+      />
+      <button onClick={saveUser}>Save User</button>
+      <button onClick={showUsers}>Show Users</button>
     </div>
   );
 }
